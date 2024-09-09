7e610cc60520a6564f9703e7b18f71e6a5ad4336
baesh <baesh6778@gmail.com>
2024-09-09 14:17:37 +0900

    09.09

--- miniproject/src/App.js
+++ miniproject/src/App.js
@@ -20,8 +20,8 @@ function App() {
   const user = [
     { id: "1", password: "1", email: "123@naver.com", name: "박지훈" },
   ];
-  const [questions, setquestions] = useState([
-    { text: "당신은 새로운 아이디어를 실험하는 것을 좋아하나요?", type: "EI" },
+  const [questions, setquestions] = useState([{ 
+    text: "당신은 새로운 아이디어를 실험하는 것을 좋아하나요?", type: "EI" },
     { text: "당신은 사람들과의 대화에서 에너지를 얻나요?", type: "EI" },
     { text: "당신은 주어진 사실과 정보에 기반해 결정을 내리나요?", type: "SN" },
     { text: "당신은 전체적인 그림보다는 세부 사항에 중점을 두나요?", type: "SN" },
